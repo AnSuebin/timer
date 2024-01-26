85f9fe7deb326a73c2507cc186cb914678ad4267
Suebin An <jejupalette@naver.com>
2024-01-26 21:48:44 +0900

    styles : 홈 페이지 내 timer loyout 추가

--- src/components/pages/home/HomeTemplate.tsx
+++ src/components/pages/home/HomeTemplate.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 interface Props {
   Current: React.ReactElement;
+  Timer: React.ReactElement;
 }
 
-const HomeTemplate: React.FC<Props> = ({ Current }) => {
-  return <main className=" p-6">{Current}</main>;
+const HomeTemplate: React.FC<Props> = ({ Current, Timer }) => {
+  return (
+    <main className=" p-6 flex flex-col gap-9">
+      {Current}
+      {Timer}
+    </main>
+  );
 };
 
 export default HomeTemplate;
