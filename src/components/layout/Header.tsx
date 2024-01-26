a5ca3c1ce2c2ead3c03e451143f2837d727ca85d
Suebin An <jejupalette@naver.com>
2024-01-26 19:37:36 +0900

    styles : header padding

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Header = () => {
   return (
-    <header className={`h-header max-mobile:h-mobileHeader flex justify-between items-center`}>
+    <header className="h-header max-mobile:h-mobileHeader flex justify-between items-center p-4">
       <div className="text-lg font-extrabold flex items-center gap-1">
         <IoTimerSharp />
         Pomodoro
