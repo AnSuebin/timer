c996a8f44e1a3ae71a581718603e08ff37bb7089
Suebin An <jejupalette@naver.com>
2024-01-26 19:54:51 +0900

    styles : 모바일 반응형 추가

--- src/components/container/MobileContainer.tsx
+++ src/components/container/MobileContainer.tsx
@@ -16,17 +16,17 @@ const MobileContainer: React.FC<React.PropsWithChildren<Props>> = ({
   children,
 }) => {
   const { isMobile } = useWindowSize();
-  const bottomPadding = Footer && 'mb-[100px]';
-  const mainTopMargin = isMobile ? 'pt-[50px]' : 'pt-[64px]';
+  const mainTopPadding = isMobile ? 'pt-[50px]' : 'pt-[64px]';
+  const mainBottomPadding = Footer && 'pb-[100px]';
 
   return (
-    <div className={'min-h-screen w-full bg-white'}>
+    <div className={'min-h-screen w-screen bg-white'}>
       {MetaHead}
       <header className="fixed top-0 left-1/2 translate-x-[-50%] z-[999] w-mobileWidth max-mobile:w-full">
         {Header}
       </header>
       <main
-        className={`${mainTopMargin} ${bottomPadding} mx-auto w-mobileWidth min-h-[calc(100vh)]  shadow-inherit`}>
+        className={`${mainTopPadding} ${mainBottomPadding} mx-auto w-mobileWidth min-h-screen shadow-md max-mobile:w-full`}>
         {children}
       </main>
       <footer className="fixed bottom-0 left-1/2 translate-x-[-50%] w-mobileWidth max-mobile:w-full">
