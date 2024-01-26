24c22c7f1519b670deeb65cea1dae8dcd76dee2b
Suebin An <jejupalette@naver.com>
2024-01-26 17:38:49 +0900

    styles : header 디자인

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { IoTimerSharp } from 'react-icons/io5';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Header = () => {
   return (
-    <header
-      className={`h-header max-mobile:h-mobileHeader bg-red-200 flex justify-between items-center `}>
-      header
+    <header className={`h-header max-mobile:h-mobileHeader flex justify-between items-center`}>
+      <div className="text-lg font-extrabold flex items-center gap-1">
+        <IoTimerSharp />
+        Pomodoro
+      </div>
+      <RxHamburgerMenu />
     </header>
   );
 };
