dc1662904e218d99ed79df346a52a14822b4b9fb
Suebin An <jejupalette@naver.com>
2024-01-26 19:37:52 +0900

    styles : footer 디자인

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div className=" h-footer">footer</div>;
+  return (
+    <footer className=" h-footer flex justify-end bg-slate-100 p-4">
+      <p className="text-slate-600 font-light text-xs">©PomodoroTimer 2024. All Rights Reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
